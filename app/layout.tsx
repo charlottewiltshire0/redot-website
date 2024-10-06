80669be54ba9ffdeaeedb5e98f0c399bf1fb30ad
StarStudio <charlottewiltshire27@outlook.com>
2024-10-06 16:00:09 +0300

    [REFACTOR]: Update font import and layout styles
    Changes:
    - Replaced local font imports for GeistSans and GeistMono with Google font JetBrains Mono.
    - Adjusted the body class to utilize the new font class for styling.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,11 @@
 import type { Metadata } from 'next';
-import localFont from 'next/font/local';
+import { JetBrains_Mono } from 'next/font/google';
 import './globals.css';
 import { ThemeProvider } from 'next-themes';
 
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-});
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
+const jetBrainsMono = JetBrains_Mono({
+  subsets: ['latin'],
+  display: 'swap',
 });
 
 export const metadata: Metadata = {
@@ -28,9 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${jetBrainsMono.className} antialiased`}>
         <ThemeProvider
           attribute='class'
           defaultTheme='dark'
