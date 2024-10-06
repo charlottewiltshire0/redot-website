0474efa3c2ed2a65f1ecf872e8a21f39fadf3cae
StarStudio <charlottewiltshire27@outlook.com>
2024-10-06 22:36:00 +0300

    [UPDATE]: Adjust background color for dark mode on the home page
    - Updated the `div` container to use `dark:bg-black` instead of `dark:bg-brand`.
    - Ensures a consistent black background for dark mode across the homepage.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,7 @@ import Hero from '@/components/hero';
 export default function Home() {
   return (
     <>
-      <div className='dark:bg-brand relative h-screen overflow-hidden bg-white px-2 pb-40 pt-20 md:px-4 md:pt-20 lg:px-8'>
+      <div className='relative h-screen overflow-hidden bg-white px-2 pb-40 pt-20 dark:bg-black md:px-4 md:pt-20 lg:px-8'>
         <Hero />
       </div>
     </>
