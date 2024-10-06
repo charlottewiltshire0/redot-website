161fc60e1778248987e48b746ce991aee98244f2
StarStudio <charlottewiltshire27@outlook.com>
2024-10-06 15:03:01 +0300

    [ADDED]: Add links and social media data for navigation
    Changes:
    - Created `index.tsx` file in the `data` directory to export navigation links and social media information.
    - Defined an array of `links` for navigation, including paths for Features, Showcase, Blog, Community, and Assets.
    - Added a `social` array containing links to Discord and Twitter for community engagement.

---
+++ data/index.tsx
@@ -0,0 +1,33 @@
+export const links = [
+  {
+    label: 'Features',
+    href: '/features/',
+  },
+  {
+    label: 'Showcase',
+    href: '/showcase/',
+  },
+  {
+    label: 'Blog',
+    href: '/blog/',
+  },
+  {
+    label: 'Community',
+    href: '/community/',
+  },
+  {
+    label: 'Assets',
+    href: '/asset/',
+  },
+];
+
+export const social = [
+  {
+    label: 'Discord',
+    href: 'https://discord.gg/redot',
+  },
+  {
+    label: 'Twitter',
+    href: 'https://x.com/redotengine',
+  },
+];
