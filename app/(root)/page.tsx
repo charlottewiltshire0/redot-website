010c44765a37157cb6546610d8da106e7f461092
StarStudio <charlottewiltshire27@outlook.com>
2024-10-06 15:01:00 +0300

    [FEAT]: Add Home page layout with Hero and Header components
    Changes:
    - Created a new `Home` page in `page.tsx` under the `(root)` directory.
    - Integrated the `Header` and `Hero` components into the page layout.
    - Applied responsive padding and background styles to the page container.

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import Hero from '@/components/Hero';
+import Header from '@/components/ui/header';
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <div className='dark:bg-brand relative overflow-hidden bg-white px-2 pb-40 pt-20 md:px-4 md:pt-20 lg:px-8'>
+        <Hero />
+      </div>
+    </>
+  );
+}
