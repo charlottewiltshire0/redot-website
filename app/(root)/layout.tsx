09c5232cb34b6cedab4d2b3f1030b09747cfe016
StarStudio <charlottewiltshire27@outlook.com>
2024-10-06 16:09:05 +0300

    [REFACTOR]: Introduce RootLayout component
    - Created `RootLayout` to encapsulate the header and children components for cleaner structure.
    - Removed direct `Header` import from `page.tsx`, ensuring header is rendered through `RootLayout`.
    - Improved code organization for better maintainability and readability.

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,14 @@
+import Header from '@/components/ui/header';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <Header />
+      {children}
+    </>
+  );
+}
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,8 @@
 import Hero from '@/components/hero';
-import Header from '@/components/ui/header';
 
 export default function Home() {
   return (
     <>
-      <Header />
       <div className='dark:bg-brand relative h-screen overflow-hidden bg-white px-2 pb-40 pt-20 md:px-4 md:pt-20 lg:px-8'>
         <Hero />
       </div>
