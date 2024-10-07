ac2547a2720c556304096ba9116c43ffee50360a
StarStudio <charlottewiltshire27@outlook.com>
2024-10-07 12:27:03 +0300

    [FEATURE]: Added news data for Godot updates
    - Introduced a new `news` array to store news articles related to Godot.
    - Each article includes a title, description, header image, icon, and link.
    - Imported necessary icons and the Skeleton component for potential loading states.

--- data/index.tsx
+++ data/index.tsx
@@ -1,3 +1,13 @@
+import {
+  IconArrowWaveRightUp,
+  IconClipboardCopy,
+  IconFileBroken,
+  IconSignature,
+  IconTableColumn,
+} from '@tabler/icons-react';
+import { Skeleton } from '@/components/ui/skeleton';
+import Image from 'next/image';
+
 export const links = [
   {
     label: 'Features',
@@ -29,5 +39,66 @@ export const social = [
   {
     label: 'Twitter',
     href: 'https://x.com/redotengine',
+  },
+];
+
+export const news = [
+  {
+    title: 'Dev snapshot: Godot 4.4 dev 3',
+    description:
+      "We haven't slowed down and are bringing you another feature-packed update!",
+    header: (
+      <Image
+        fill
+        src='https://godotengine.org/storage/blog/covers/dev-snapshot-godot-4-4-dev-3.webp'
+        alt='Dev snapshot: Godot 4.4 dev 3'
+        className='rounded-lg object-cover'
+      />
+    ),
+    href: 'https://godotengine.org/article/dev-snapshot-godot-4-4-dev-3/',
+    icon: <IconClipboardCopy className='h-4 w-4 text-neutral-500' />,
+  },
+  {
+    title: 'Godot XR update - October 2024',
+    description: 'Looking back at a year of XR development.',
+    header: (
+      <Image
+        fill
+        src='https://godotengine.org/storage/blog/covers/dev-snapshot-godot-4-4-dev-3.webp'
+        alt='Dev snapshot: Godot 4.4 dev 3'
+        className='rounded-lg object-cover'
+      />
+    ),
+    icon: <IconFileBroken className='h-4 w-4 text-neutral-500' />,
+    href: 'https://godotengine.org/article/godot-xr-update-oct-2024/',
+  },
+  {
+    title: 'Godot SDK Integrations',
+    description:
+      'A new GitHub organization to centralize the community efforts to provide support for third-party SDK...',
+    header: (
+      <Image
+        fill
+        src='https://godotengine.org/storage/blog/covers/dev-snapshot-godot-4-4-dev-3.webp'
+        alt='Dev snapshot: Godot 4.4 dev 3'
+        className='rounded-lg object-cover'
+      />
+    ),
+    icon: <IconSignature className='h-4 w-4 text-neutral-500' />,
+    href: 'https://godotengine.org/article/godot-sdk-integrations/',
+  },
+  {
+    title: 'Godot Rendering Priorities: September 2024',
+    description: "4.3 is out. It's time for an update!",
+    header: (
+      <Image
+        fill
+        src='https://godotengine.org/storage/blog/covers/dev-snapshot-godot-4-4-dev-3.webp'
+        alt='Dev snapshot: Godot 4.4 dev 3'
+        className='rounded-lg object-cover'
+      />
+    ),
+    icon: <IconTableColumn className='h-4 w-4 text-neutral-500' />,
+    href: 'https://godotengine.org/article/rendering-priorities-september-2024/',
   },
 ];
