d18861635483ba79140724571df44350095fcd5d
StarStudio <charlottewiltshire27@outlook.com>
2024-10-07 13:11:05 +0300

    [ADD]: Implement BentoGrid and BentoCard components
    - Added BentoGrid component for layout management.
    - Introduced BentoCard component for displaying cards with customizable content, including background, icons, descriptions, and call-to-action buttons.
    - Integrated responsive styles and hover effects for enhanced user interaction.

--- components/ui/bento-grid.tsx
+++ components/ui/bento-grid.tsx
@@ -1,17 +1,21 @@
-import { cn } from '@/lib/utils';
+import { ReactNode } from "react";
+import { ArrowRightIcon } from "@radix-ui/react-icons";
 
-export const BentoGrid = ({
-  className,
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+
+const BentoGrid = ({
   children,
+  className,
 }: {
+  children: ReactNode;
   className?: string;
-  children?: React.ReactNode;
 }) => {
   return (
     <div
       className={cn(
-        'mx-auto grid max-w-7xl grid-cols-1 gap-4 md:auto-rows-[18rem] md:grid-cols-2',
-        className
+        "grid w-full auto-rows-[22rem] grid-cols-3 gap-4",
+        className,
       )}
     >
       {children}
@@ -19,38 +23,57 @@ export const BentoGrid = ({
   );
 };
 
-export const BentoGridItem = ({
+const BentoCard = ({
+  name,
   className,
-  title,
+  background,
+  Icon,
   description,
-  header,
-  icon,
+  href,
+  cta,
 }: {
-  className?: string;
-  title?: string | React.ReactNode;
-  description?: string | React.ReactNode;
-  header?: React.ReactNode;
-  icon?: React.ReactNode;
-}) => {
-  return (
+  name: string;
+  className: string;
+  background: ReactNode;
+  Icon: any;
+  description: string;
+  href: string;
+  cta: string;
+}) => (
+  <div
+    key={name}
+    className={cn(
+      "group relative col-span-3 flex flex-col justify-between overflow-hidden rounded-xl",
+      // light styles
+      "bg-white [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)]",
+      // dark styles
+      "transform-gpu dark:bg-black dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]",
+      className,
+    )}
+  >
+    <div>{background}</div>
+    <div className="pointer-events-none z-10 flex transform-gpu flex-col gap-1 p-6 transition-all duration-300 group-hover:-translate-y-10">
+      <Icon className="h-12 w-12 origin-left transform-gpu text-neutral-700 transition-all duration-300 ease-in-out group-hover:scale-75" />
+      <h3 className="text-xl font-semibold text-neutral-700 dark:text-neutral-300">
+        {name}
+      </h3>
+      <p className="max-w-lg text-neutral-400">{description}</p>
+    </div>
+
     <div
       className={cn(
-        'group/bento row-span-1 flex justify-between space-x-4 rounded-xl border border-transparent bg-white p-4 shadow-input transition duration-200 hover:shadow-xl dark:border-white/[0.2] dark:bg-black dark:shadow-none md:flex-col md:space-y-4',
-        className
+        "pointer-events-none absolute bottom-0 flex w-full translate-y-10 transform-gpu flex-row items-center p-4 opacity-0 transition-all duration-300 group-hover:translate-y-0 group-hover:opacity-100",
       )}
     >
-      <div className='relative hidden h-full w-full md:block'>{header}</div>
-      <div>
-        <div className='transition duration-200 group-hover/bento:translate-x-2'>
-          {icon}
-          <div className='mb-2 mt-2 font-sans font-bold text-neutral-600 dark:text-neutral-200'>
-            {title}
-          </div>
-          <div className='font-sans text-xs font-normal text-neutral-600 dark:text-neutral-300'>
-            {description}
-          </div>
-        </div>
-      </div>
+      <Button variant="ghost" asChild size="sm" className="pointer-events-auto">
+        <a href={href}>
+          {cta}
+          <ArrowRightIcon className="ml-2 h-4 w-4" />
+        </a>
+      </Button>
     </div>
-  );
-};
+    <div className="pointer-events-none absolute inset-0 transform-gpu transition-all duration-300 group-hover:bg-black/[.03] group-hover:dark:bg-neutral-800/10" />
+  </div>
+);
+
+export { BentoCard, BentoGrid };
