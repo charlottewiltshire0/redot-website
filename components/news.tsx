ce77d0d99953d852daa764fc5672975e61a03ad0
StarStudio <charlottewiltshire27@outlook.com>
2024-10-07 13:12:29 +0300

    [UPDATE]: Refactor News component to use BentoGridNews and add More News button
    - Replaced BentoGrid with BentoGridNews for better organization of news items.
    - Adjusted section layout with additional spacing and improved responsiveness.
    - Added a "Want to see more news?" message and a button linking to the blog for expanded news content.
    - Fixed typo in the text color class from 'text-neutrl-900' to 'text-neutral-900'.

--- components/news.tsx
+++ components/news.tsx
@@ -1,15 +1,16 @@
-import { BentoGrid, BentoGridItem } from '@/components/ui/bento-grid';
+import { BentoGridNews, BentoGridItem } from '@/components/ui/bento-grid-news';
 import { news } from '@/data';
 import Link from 'next/link';
+import { Button } from '@/components/ui/button';
 
 const News = () => {
   return (
-    <section className='relative mx-auto mt-16 w-full max-w-[84rem] md:mt-0'>
-      <div>
-        <h2 className='text-neutrl-900 mb-4 text-3xl font-medium tracking-tight dark:text-white sm:text-left'>
+    <section className='relative mx-auto mt-16 w-full max-w-[84rem] px-4 md:mt-0 md:px-0'>
+      <div className='space-y-4'>
+        <h2 className='text-neutrl-900 text-3xl font-medium tracking-tight dark:text-white sm:text-left'>
           Latest news
         </h2>
-        <BentoGrid className='grid max-w-[84rem] grid-cols-1 gap-4 md:grid-cols-2 md:grid-rows-3'>
+        <BentoGridNews className='grid max-w-[84rem] grid-cols-1 gap-4 md:grid-cols-2 md:grid-rows-3'>
           <BentoGridItem
             key={0}
             title={news[0].title}
@@ -30,7 +31,15 @@ const News = () => {
               className={index === 2 ? 'col-start-auto md:col-start-2' : ''}
             />
           ))}
-        </BentoGrid>
+        </BentoGridNews>
+        <div className='flex flex-row items-center justify-between px-0 md:px-8'>
+          <h2 className='relative w-[50%] text-left text-sm leading-loose tracking-wide text-zinc-500 antialiased dark:text-zinc-300 sm:text-xl'>
+            Want to see more news?
+          </h2>
+          <Button variant='secondary' asChild>
+            <Link href='/blog'>More News</Link>
+          </Button>
+        </div>
       </div>
     </section>
   );
