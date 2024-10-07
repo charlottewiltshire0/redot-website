f2dd4241673f788790c757acdc21718e4f115f3f
StarStudio <charlottewiltshire27@outlook.com>
2024-10-07 12:26:40 +0300

    [CONFIG]: Added remote image loading patterns for Godot Engine
    - Configured remote image patterns in next.config.mjs to allow images from https://godotengine.org/storage/blog/covers/**.
    - This change enables the use of external images in the application, improving flexibility in content management.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'godotengine.org',
+        port: '',
+        pathname: '/storage/blog/covers/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
