ba22d8cd39c9d197a84ff2de2cff8f81b271dd26
StarStudio <charlottewiltshire27@outlook.com>
2024-10-11 23:19:17 +0300

    [ENHANCEMENT]: Update Start component layout and add Get Started button
    - Refactored layout of the Start component to center-align the introductory text.
    - Added a "Get Started" button that links directly to the download page (`/download`).
    - Improved visual hierarchy and user engagement with a clearer call to action.

--- components/start.tsx
+++ components/start.tsx
@@ -1,4 +1,6 @@
 import dynamic from 'next/dynamic';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 const WavyBackground = dynamic(() =>
   import('@/components/ui/wavy-background').then((mod) => mod.WavyBackground)
@@ -8,13 +10,22 @@ const Start = () => {
   return (
     <section className='relative'>
       <WavyBackground className='mx-auto max-w-4xl pb-40'>
-        <p className='inter-var text-center text-2xl font-bold text-white md:text-4xl lg:text-7xl'>
-          Ready to Create?
-        </p>
-        <p className='inter-var mt-4 text-center text-base font-normal text-white md:text-lg'>
-          Create captivating 2D and 3D games with Redot Engine for
-          cross-platform immersive experiences.
-        </p>
+        <div className='flex flex-col items-center justify-center space-y-4'>
+          <div>
+            <p className='inter-var text-center text-2xl font-bold text-white md:text-4xl lg:text-7xl'>
+              Ready to Create?
+            </p>
+            <p className='inter-var mt-4 text-center text-base font-normal text-white md:text-lg'>
+              Create captivating 2D and 3D games with Redot Engine for
+              cross-platform immersive experiences.
+            </p>
+          </div>
+          <div>
+            <Button asChild>
+              <Link href='/download'>Get Started</Link>
+            </Button>
+          </div>
+        </div>
       </WavyBackground>
     </section>
   );
