4714bd2df2807eb0eebfd2d47d36fab09923aa1e
StarStudio <charlottewiltshire27@outlook.com>
2024-10-11 18:13:23 +0300

    [UPDATE]: Enhance metadata in layout.tsx
    Changes:
    - Added `creator` field with value 'Redot Engine'.
    - Set `appleWebApp` to true to indicate support for Apple web apps.
    - Added `applicationName` with a detailed description for better app representation.

---
+++ components/download.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import GradualSpacing from '@/components/ui/gradual-spacing';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import { CoolMode } from '@/components/ui/cool-mode';
+
+const Download = () => {
+  const platform = window.navigator.userAgent.includes('Win')
+    ? 'Windows'
+    : window.navigator.userAgent.includes('Mac')
+      ? 'Mac'
+      : window.navigator.userAgent.includes('Linux')
+        ? 'Linux'
+        : 'your platform';
+
+  const platformDownloadLink =
+    platform === 'Windows'
+      ? 'https://github.com/Redot-Engine/redot-engine/releases/download/2024101114/redot-windows-template.zip'
+      : platform === 'Mac'
+        ? 'https://github.com/Redot-Engine/redot-engine/releases/download/2024101114/macos-template.zip'
+        : platform === 'Linux'
+          ? 'https://github.com/Redot-Engine/redot-engine/releases/download/2024101114/redot-linux-template.zip'
+          : 'https://github.com/Redot-Engine/redot-engine/releases';
+
+  return (
+    <section>
+      <div className='relative z-20 mx-auto mt-16 w-full max-w-[84rem]'>
+        <div className='flex flex-col items-center px-8 xl:px-0'>
+          <div className='flex items-center justify-center gap-10 pb-10 md:pb-40'>
+            <div className='flex flex-col items-start px-8 xl:px-0'>
+              <h1 className='relative mb-12 max-w-4xl text-left text-4xl font-bold text-zinc-700 dark:text-zinc-100 md:text-7xl'>
+                <span className='inline-block break-words text-center align-top no-underline'>
+                  Download Redot for
+                  <GradualSpacing
+                    className='font-display text-center text-4xl font-bold -tracking-widest text-black dark:text-white md:text-7xl md:leading-[5rem]'
+                    text={platform}
+                  />
+                </span>
+              </h1>
+              <div className='mb-4 flex w-full flex-col items-center justify-center space-y-2 sm:flex-row sm:justify-start sm:space-x-4 sm:space-y-0 sm:px-0 md:px-12'>
+                <CoolMode>
+                  <Button className='w-full' size='lg' asChild>
+                    <Link href={platformDownloadLink} target='__blank'>
+                      Redot Engine
+                    </Link>
+                  </Button>
+                </CoolMode>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Download;
