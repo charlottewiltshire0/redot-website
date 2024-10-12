2f8190a90d90f0da0d3b70346988d282eab3c850
StarStudio <charlottewiltshire27@outlook.com>
2024-10-13 00:13:49 +0300

    [UPDATE]: Enhance ArticleItemList with improved icons and background images
    Changes:
    - Replaced the `IconDeviceGamepad2` with `IconNews` for better representation of articles.
    - Added `Image` component to display article background images dynamically.
    - Updated the `BentoCard` class name for improved grid layout styling.
    - Changed the call-to-action text from 'Learn more' to 'Read more' for consistency.

--- components/ui/article-list-item.tsx
+++ components/ui/article-list-item.tsx
@@ -1,6 +1,7 @@
 import type { ArticleItem } from '@/types';
 import { BentoCard, BentoGrid } from '@/components/ui/bento-grid';
-import { IconDeviceGamepad2 } from '@tabler/icons-react';
+import { IconNews } from '@tabler/icons-react';
+import Image from 'next/image';
 
 interface Props {
   articles: ArticleItem[];
@@ -13,11 +14,19 @@ const ArticleItemList = ({ articles }: Props) => {
         {articles.map((article, id) => (
           <BentoCard
             name={article.title}
-            background={<div>123</div>}
-            Icon={IconDeviceGamepad2}
-            description='123'
+            className='col-span-3 lg:col-span-1'
+            background={
+              <Image
+                className='absolute -right-20 -top-20 object-cover transition-all duration-300 ease-out [mask-image:linear-gradient(to_top,transparent_10%,#000_100%)] group-hover:scale-105'
+                src={article.background}
+                alt={article.title}
+                fill
+              />
+            }
+            Icon={IconNews}
+            description={article.description}
             href={`/blog/${article.id}`}
-            cta='Learn more'
+            cta='Read more'
             key={id}
           />
         ))}
