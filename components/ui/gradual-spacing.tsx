62785aa488f5c7fb2319746204a433e2e429c3d2
StarStudio <charlottewiltshire27@outlook.com>
2024-10-12 11:16:59 +0300

    prettier

--- components/ui/gradual-spacing.tsx
+++ components/ui/gradual-spacing.tsx
@@ -1,8 +1,8 @@
-"use client";
+'use client';
 
-import { AnimatePresence, motion, Variants } from "framer-motion";
+import { AnimatePresence, motion, Variants } from 'framer-motion';
 
-import { cn } from "@/lib/utils";
+import { cn } from '@/lib/utils';
 
 interface GradualSpacingProps {
   text: string;
@@ -23,19 +23,19 @@ export default function GradualSpacing({
   className,
 }: GradualSpacingProps) {
   return (
-    <div className="flex justify-center space-x-1">
+    <div className='flex justify-center space-x-1'>
       <AnimatePresence>
-        {text.split("").map((char, i) => (
+        {text.split('').map((char, i) => (
           <motion.h1
             key={i}
-            initial="hidden"
-            animate="visible"
-            exit="hidden"
+            initial='hidden'
+            animate='visible'
+            exit='hidden'
             variants={framerProps}
             transition={{ duration, delay: i * delayMultiple }}
-            className={cn("drop-shadow-sm ", className)}
+            className={cn('drop-shadow-sm', className)}
           >
-            {char === " " ? <span>&nbsp;</span> : char}
+            {char === ' ' ? <span>&nbsp;</span> : char}
           </motion.h1>
         ))}
       </AnimatePresence>
