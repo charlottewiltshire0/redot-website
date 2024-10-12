443c6326105c4aa62c68a05b1bbf71917c8a56d8
StarStudio <charlottewiltshire27@outlook.com>
2024-10-12 11:16:23 +0300

    [ADD]: Add getPlatform utility function for platform detection
    Changes:
    - Implemented `getPlatform` function to detect the user's operating system based on the browser's user agent.
    - Returns 'Windows', 'macOS', 'Linux', or 'Unknown' depending on the detected platform.

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,20 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const getPlatform = () => {
+  if (typeof window !== 'undefined') {
+    const userAgent = window.navigator.userAgent;
+
+    if (/Windows/i.test(userAgent)) {
+      return 'Windows';
+    }
+    if (/Mac/i.test(userAgent)) {
+      return 'macOS';
+    }
+    if (/Linux/i.test(userAgent)) {
+      return 'Linux';
+    }
+  }
+  return 'Unknown';
+};
