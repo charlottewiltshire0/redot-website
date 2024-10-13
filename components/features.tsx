de69f67faaa5f440565cce028453cb8b29fedc00
StarStudio <charlottewiltshire27@outlook.com>
2024-10-13 20:18:48 +0300

    [REMOVE]: Eliminate hardcoded news data
    Changes:
    - Removed the static `news` array containing predefined news articles.
    - Updated links to features and showcase sections to point to the correct anchors.

--- components/features.tsx
+++ components/features.tsx
@@ -3,7 +3,10 @@ import { features } from '@/data';
 
 const Features = () => {
   return (
-    <section className='relative mx-auto mt-24 w-full max-w-[84rem] px-4 md:mt-32 md:px-0'>
+    <section
+      id='features'
+      className='relative mx-auto mt-24 w-full max-w-[84rem] px-4 md:mt-32 md:px-0'
+    >
       <div className='space-y-4'>
         <h2 className='text-neutrl-900 text-3xl font-medium tracking-tight dark:text-white sm:text-left'>
           Next-Gen Game Design
--- data/index.tsx
+++ data/index.tsx
@@ -26,7 +26,7 @@ const AnimatedBeamMultipleOutputCrossPlatform = dynamic(() =>
 export const links = [
   {
     label: 'Features',
-    href: '/features/',
+    href: '/#features',
   },
   {
     label: 'Showcase',
@@ -54,67 +54,6 @@ export const social = [
   {
     label: 'Twitter',
     href: 'https://x.com/redotengine',
-  },
-];
-
-export const news = [
-  {
-    title: 'Dev snapshot: Godot 4.4 dev 3',
-    description:
-      "We haven't slowed down and are bringing you another feature-packed update!",
-    header: (
-      <Image
-        fill
-        src='https://godotengine.org/storage/blog/covers/dev-snapshot-godot-4-4-dev-3.webp'
-        alt='Dev snapshot: Godot 4.4 dev 3'
-        className='rounded-lg object-cover'
-      />
-    ),
-    href: 'https://godotengine.org/article/dev-snapshot-godot-4-4-dev-3/',
-    icon: <IconClipboardCopy className='h-4 w-4 text-neutral-500' />,
-  },
-  {
-    title: 'Godot XR update - October 2024',
-    description: 'Looking back at a year of XR development.',
-    header: (
-      <Image
-        fill
-        src='https://godotengine.org/storage/blog/covers/dev-snapshot-godot-4-4-dev-3.webp'
-        alt='Dev snapshot: Godot 4.4 dev 3'
-        className='rounded-lg object-cover'
-      />
-    ),
-    icon: <IconFileBroken className='h-4 w-4 text-neutral-500' />,
-    href: 'https://godotengine.org/article/godot-xr-update-oct-2024/',
-  },
-  {
-    title: 'Godot SDK Integrations',
-    description:
-      'A new GitHub organization to centralize the community efforts to provide support for third-party SDK...',
-    header: (
-      <Image
-        fill
-        src='https://godotengine.org/storage/blog/covers/dev-snapshot-godot-4-4-dev-3.webp'
-        alt='Dev snapshot: Godot 4.4 dev 3'
-        className='rounded-lg object-cover'
-      />
-    ),
-    icon: <IconSignature className='h-4 w-4 text-neutral-500' />,
-    href: 'https://godotengine.org/article/godot-sdk-integrations/',
-  },
-  {
-    title: 'Godot Rendering Priorities: September 2024',
-    description: "4.3 is out. It's time for an update!",
-    header: (
-      <Image
-        fill
-        src='https://godotengine.org/storage/blog/covers/dev-snapshot-godot-4-4-dev-3.webp'
-        alt='Dev snapshot: Godot 4.4 dev 3'
-        className='rounded-lg object-cover'
-      />
-    ),
-    icon: <IconTableColumn className='h-4 w-4 text-neutral-500' />,
-    href: 'https://godotengine.org/article/rendering-priorities-september-2024/',
   },
 ];
 
@@ -193,7 +132,7 @@ export const footer_links_first = [
   },
   {
     name: 'Features',
-    href: '/features',
+    href: '/#features',
   },
   {
     name: 'Source Code',
