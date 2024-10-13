e0f7c131916b598bc642415e84b7ef1a2fcf3fd6
StarStudio <charlottewiltshire27@outlook.com>
2024-10-13 22:00:55 +0300

    [REFINE]: Update layout margins and padding in Blog and Showcase pages
    Changes:
    - Removed top margin (`mt-16`) from the section in both Blog and Showcase pages to streamline the layout.
    - Adjusted padding classes to enhance responsiveness without altering overall structure.

--- app/(root)/showcase/page.tsx
+++ app/(root)/showcase/page.tsx
@@ -3,12 +3,12 @@ import { Skeleton } from '@/components/ui/skeleton';
 export default function Blog() {
   return (
     <>
-      <div className='flex flex-col overflow-hidden bg-white px-2 pb-40 pt-20 dark:bg-black md:px-4 md:pt-20 lg:px-8'>
+      <div className='flex flex-col overflow-hidden bg-white px-2 pb-40 pt-20 dark:bg-black md:px-4 lg:px-8'>
         <div className='absolute left-0 top-0 flex h-screen w-full items-center justify-center bg-white bg-grid-black/[0.2] dark:bg-black dark:bg-grid-white/[0.03]'>
           {/* Radial gradient for the container to give a faded look */}
           <div className='pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black'></div>
         </div>
-        <section className='relative z-20 mx-auto mt-16 h-screen w-full max-w-[84rem]'>
+        <section className='relative z-20 mx-auto h-screen w-full max-w-[84rem]'>
           <h2 className='mb-2 text-center text-5xl font-bold leading-[1.2] tracking-tighter text-foreground'>
             Showcase
           </h2>
