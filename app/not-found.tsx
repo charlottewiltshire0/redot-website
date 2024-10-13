a254d942683acf3e58acea8b25b5795c87f2e328
StarStudio <charlottewiltshire27@outlook.com>
2024-10-13 21:34:59 +0300

    Index: app/not-found.tsx IDEA additional info: Subsystem: com.intellij.openapi.diff.impl.patch.CharsetEP <+>UTF-8 =================================================================== diff --git a/app/not-found.tsx b/app/not-found.tsx --- a/app/not-found.tsx	(revision d81cab7cd38609e3d11a319080beb098390bc143) +++ b/app/not-found.tsx	(date 1728844469509) @@ -1,3 +1,5 @@ +import Image from 'next/image'; +  export default function NotFound() {    return (      <div className='relative z-10 flex h-screen flex-col justify-center overflow-hidden bg-blue-600 px-6 text-lg text-white sm:px-36 sm:text-xl'> @@ -15,7 +17,12 @@          Don&#39;t worry, it happens to the best of us! You might want to check          the URL, or maybe even your sense of direction.        </div> -      <img className='h-24 w-24' alt='' aria-hidden='true' src='/qrcode.webp' /> +      <Image +        className='h-24 w-24' +        alt='' +        aria-hidden='true' +        src='/qrcode.webp' +      />      </div>    );  }

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 export default function NotFound() {
   return (
     <div className='relative z-10 flex h-screen flex-col justify-center overflow-hidden bg-blue-600 px-6 text-lg text-white sm:px-36 sm:text-xl'>
@@ -15,7 +17,12 @@ export default function NotFound() {
         Don&#39;t worry, it happens to the best of us! You might want to check
         the URL, or maybe even your sense of direction.
       </div>
-      <img className='h-24 w-24' alt='' aria-hidden='true' src='/qrcode.webp' />
+      <Image
+        className='h-24 w-24'
+        alt=''
+        aria-hidden='true'
+        src='/qrcode.webp'
+      />
     </div>
   );
 }
