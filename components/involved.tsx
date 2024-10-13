a35625a10a655e5b6803985ea52fc9cc26c7ab61
StarStudio <charlottewiltshire27@outlook.com>
2024-10-13 20:49:12 +0300

    [ADD]: Implement Involved section for community engagement
    Changes:
    - Created a new `Involved` component to highlight ways users can get involved in the community.
    - Added a grid display of involvement opportunities using `BentoGridNews` and `BentoGridItem`.
    - Included a "Learn more" button linking to the community page.

---
+++ components/involved.tsx
@@ -0,0 +1,35 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import { BentoGridItem, BentoGridNews } from '@/components/ui/bento-grid-news';
+import { involved } from '@/data';
+
+const Involved = () => {
+  return (
+    <section className='relative mx-auto w-full max-w-[84rem] pt-16 md:mt-0 md:px-0'>
+      <div className='space-y-4'>
+        <div className='flex flex-row items-center justify-between'>
+          <h2 className='text-neutrl-900 text-3xl font-medium tracking-tight dark:text-white sm:text-left'>
+            Get Involved
+          </h2>
+          <Button variant='outline' asChild>
+            <Link href='/community'>Learn more</Link>
+          </Button>
+        </div>
+        <BentoGridNews>
+          {involved.map((item, i) => (
+            <BentoGridItem
+              key={i}
+              title={item.title}
+              description={item.description}
+              header={item.header}
+              icon={item.icon}
+              className={i === 3 || i === 6 ? 'md:col-span-2' : ''}
+            />
+          ))}
+        </BentoGridNews>
+      </div>
+    </section>
+  );
+};
+
+export default Involved;
